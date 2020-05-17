956066361516cf475fb7ed9edc30a55e0d59c4c8
mshivkumar <mshiv317@gmail.com>
2020-05-17 21:35:36 +0530

    add filtering logic for meal data

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,17 +1,132 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters-screen';
+
+  final Function _saveFilters;
+  final Map<String, bool> currentFilters;
+
+  FiltersScreen(this.currentFilters, this._saveFilters);
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  Widget _buildSwitchListTile(
+      {String title,
+      String subtitle,
+      bool currentValue,
+      Function updateValue}) {
+    return SwitchListTile(
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyText2,
+      ),
+      subtitle: Text(
+        subtitle,
+        style: Theme.of(context).textTheme.bodyText1,
+      ),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
+
+  @override
+  void initState() {
+    _glutenFree = widget.currentFilters['gluten'];
+    _vegetarian = widget.currentFilters['vegetarian'];
+    _vegan = widget.currentFilters['vegan'];
+    _lactoseFree = widget.currentFilters['lactose'];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Filters'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final _selectedFilters = {
+                'gluten': _glutenFree,
+                'lactose': _lactoseFree,
+                'vegan': _vegan,
+                'vegetarian': _vegetarian,
+              };
+              return widget._saveFilters(_selectedFilters);
+            },
+          )
+        ],
       ),
       drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Adjust your meal Selection",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildSwitchListTile(
+                  title: 'Gluten Free',
+                  subtitle: 'Only include gluten-free meals.',
+                  currentValue: _glutenFree,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  title: 'Lactose Free',
+                  subtitle: 'Only include lactose-free meals.',
+                  currentValue: _lactoseFree,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  title: 'Vegetarian',
+                  subtitle: 'Only include vegetarian meals.',
+                  currentValue: _vegetarian,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  title: 'Vegan',
+                  subtitle: 'Only include vegan meals.',
+                  currentValue: _vegan,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
